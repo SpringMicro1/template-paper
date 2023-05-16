83239ee67199555267d1e19cfb4f03d154a8922a
Richard Henage <rthenage@gmail.com>
2023-05-16 16:03:53 -0600

    Config Format

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,60 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-const pagesString =
-  // BEGIN JSON
-  `{"BASE":{  "website": "https://astro-paper.pages.dev/",  "author": "Sat Naing",  "desc": "A minimal, responsive and SEO-friendly Astro blog theme.",  "title": "AstroPaper",  "ogImage": "astropaper-og.jpg",  "lightAndDarkMode": true,  "postPerPage": 3, "LOGO_IMAGE_ENABLE": false,"LOGO_IMAGE_SVG": true,"LOGO_IMAGE_WIDTH": 216,"LOGO_IMAGE_HEIGHT": 46},"HOME":{"TEXT":{"Heading":"Mingalaba","Description":"AstroPaper is a minimal, responsive, accessible and SEO-friendly Astro blog theme. This theme follows best practices and provides accessibility out of the box. Light and dark mode are supported by default. Moreover, additional color schemes can also be configured.","Segue":"Read the blog posts or check README for more info."},"LINKS":{ "Link Github": { "href": "https://github.com/satnaing/astro-paper", "content": " AstroPaper on Github"}, "Link Facebook": { "href": "https://github.com/satnaing/astro-paper", "content": "AstroPaper on Facebook"}, "Link Instagram": { "href": "https://github.com/satnaing/astro-paper", "content": "AstroPaper on Instagram"}, "Link LinkedIn": { "href": "https://github.com/satnaing/astro-paper", "content": "AstroPaper on LinkedIn"}, "Link Mail": { "href": "mailto:yourmail@gmail.com", "content": "Send an email to AstroPaper"}},"IMAGES":{}},"ABOUT":{"TEXT":{"Heading":"About","Description":"AstroPaper is a minimal, responsive and SEO-friendly Astro blog theme. I designed and crafted this based on my personal blog."},"IMAGES":{"Blog Picture":{"Src":"https://astro-paper.pages.dev/assets/dev.svg.svg", "Alt":"Blog picture"}}}}`;
-// END JSON
-const pagesObject = JSON.parse(pagesString);
-export const BASE = pagesObject.BASE;
-export const HOME = pagesObject.HOME;
-export const ABOUT = pagesObject.ABOUT;
+
+export const BASE = {
+  website: "https://astro-paper.pages.dev/",
+  author: "Sat Naing",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "AstroPaper",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  LOGO_IMAGE_ENABLE: false,
+  LOGO_IMAGE_SVG: true,
+  LOGO_IMAGE_WIDTH: 216,
+  LOGO_IMAGE_HEIGHT: 46,
+};
+export const HOME = {
+  TEXT: {
+    Heading: "Mingalaba",
+    Description:
+      "AstroPaper is a minimal, responsive, accessible and SEO-friendly Astro blog theme. This theme follows best practices and provides accessibility out of the box. Light and dark mode are supported by default. Moreover, additional color schemes can also be configured.",
+    Segue: "Read the blog posts or check README for more info.",
+  },
+  LINKS: {
+    "Link Github": {
+      href: "https://github.com/satnaing/astro-paper",
+      content: " AstroPaper on Github",
+    },
+    "Link Facebook": {
+      href: "https://github.com/satnaing/astro-paper",
+      content: "AstroPaper on Facebook",
+    },
+    "Link Instagram": {
+      href: "https://github.com/satnaing/astro-paper",
+      content: "AstroPaper on Instagram",
+    },
+    "Link LinkedIn": {
+      href: "https://github.com/satnaing/astro-paper",
+      content: "AstroPaper on LinkedIn",
+    },
+    "Link Mail": {
+      href: "mailto:yourmail@gmail.com",
+      content: "Send an email to AstroPaper",
+    },
+  },
+  IMAGES: {},
+};
+export const ABOUT = {
+  TEXT: {
+    Heading: "About",
+    Description:
+      "AstroPaper is a minimal, responsive and SEO-friendly Astro blog theme. I designed and crafted this based on my personal blog.",
+  },
+  IMAGES: {
+    "Blog Picture": {
+      Src: "https://astro-paper.pages.dev/assets/dev.svg.svg",
+      Alt: "Blog picture",
+    },
+  },
+};
